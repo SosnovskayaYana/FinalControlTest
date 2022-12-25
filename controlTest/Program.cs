4f7eb3ba9b7b242a350a6be7b244dc7a0ae2f897
Yana <yan4ik999@yandex.ru>
2022-12-25 18:12:55 +0300

    добавлены метод заполнения нового массива, вызов метода, вывод результата

--- controlTest/Program.cs
+++ controlTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Программа из имеющегося массива строк создает новый массив из строк, длина которых меньше или равна 3 символа
 
 
+/// метод принимает на вход массив.
+/// подсчитывает количество элементов, подходящих под параметр. Возвращает переменную
 int CountElementsArray(string[] array2D)
 {
     int count = 0;
@@ -17,20 +19,49 @@ int CountElementsArray(string[] array2D)
 }
 
 
+/// метод принимает на вход массив и переменную. 
+/// заполняет новый массив элементами, подходящими под параметр. Возвращает новый массив
+string[] CreateNewArray(string[] array, int coutnElements)
+{
+    string[] newArray = new string[coutnElements];
+
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+
+    return newArray;
+}
+
+
 
 Console.WriteLine("Введите элементы массива через запятую и нажмите Enter ");
 
 string? temp = Console.ReadLine();
 
 
-if (temp != null)
+if (temp != null)   // проверка введенных данных
 {
 
-    string[] arrayLocal = temp.Split(",");
 
-    Console.WriteLine($"Полученный массив: [{String.Join("; ", arrayLocal)}]");
+    string[] arrayLocal = temp.Split(",");   // методом split делим строку "запятой" на элементы
 
-    int countElemArray = CountElementsArray(arrayLocal);
+    Console.WriteLine($"Полученный массив: [{String.Join("; ", arrayLocal)}]");   // выводим массив с элементами
+
+
+    int countElemArray = CountElementsArray(arrayLocal);   // вызов метода
+
+    string[] createNewArray = CreateNewArray(arrayLocal, countElemArray);   // вызов метода
+
+
+    Console.WriteLine($"new массив: [{String.Join("; ", createNewArray)}]");   // вывод результата
+
 
 }
 
