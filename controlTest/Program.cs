1de4359b84f199379c93945a6366aef48e55636e
Yana <yan4ik999@yandex.ru>
2022-12-25 17:44:55 +0300

    добавлен метод  подсчета элементов, вызов метода

--- controlTest/Program.cs
+++ controlTest/Program.cs
@@ -1,6 +1,22 @@
 ﻿// Программа из имеющегося массива строк создает новый массив из строк, длина которых меньше или равна 3 символа
 
 
+int CountElementsArray(string[] array2D)
+{
+    int count = 0;
+
+    for (int i = 0; i < array2D.Length; i++)
+    {
+        if (array2D[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
 
 Console.WriteLine("Введите элементы массива через запятую и нажмите Enter ");
 
@@ -13,6 +29,8 @@ if (temp != null)
     string[] arrayLocal = temp.Split(",");
 
     Console.WriteLine($"Полученный массив: [{String.Join("; ", arrayLocal)}]");
+
+    int countElemArray = CountElementsArray(arrayLocal);
 
 }
 
